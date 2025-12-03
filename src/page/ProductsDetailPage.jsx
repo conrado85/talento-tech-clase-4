9ec19fa26d0652da1c3096bc9e0eadaf80bcc601
conrado <carlos_ag20@hotmail.com>
2025-12-02 22:56:41 -0300

    terminando el carrito y detalle de producto

--- src/page/ProductsDetailPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ProductsDetailPage() {
-  return (
-    <div>
-      <h1>detalle del producto</h1>
-    </div>
-  )
-}
--- src/page/ProductsPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ProductsPage() {
-  return (
-    <div>
-      <h1>bienvenido a la pagina de productos</h1>
-    </div>
-  )
-}
